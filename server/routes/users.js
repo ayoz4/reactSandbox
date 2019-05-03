f08087a597bf2a6fe04e761748309be5f3f30a11
Roman <ayoz4@yandex.ru>
2019-05-03 13:38:10 +0700

    uniqueness validation added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,34 +1,59 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import isEmpty from "lodash/isEmpty";
 
-import validateInput from "../shared/validations/signup";
+import commonValidations from "../shared/validations/signup";
 import User from "../models/user";
 
 let router = express.Router();
 
-router.post("/", (req, res) => {
-  const { errors, isValid } = validateInput(req.body);
+function validateInput(data, otherValidations) {
+  let { errors } = otherValidations(data);
 
-  if (isValid) {
-    const { username, password, email } = req.body;
-    const password_digest = bcrypt.hashSync(password, 10);
-
-    User.forge(
-      {
-        username,
-        email,
-        password_digest
-      },
-      {
-        hasTimestamps: true
+  return User.query({
+    where: { email: data.email },
+    orWhere: { username: data.username }
+  })
+    .fetch()
+    .then(user => {
+      if (user) {
+        if (user.get("username") === data.username) {
+          errors.username = "There is user with such username";
+        }
+        if (user.get("email") === data.email) {
+          errors.email = "There is user with such email";
+        }
       }
-    )
-      .save()
-      .then(user => res.json({ success: true }))
-      .catch(err => res.status(500).json({ error: err }));
-  } else {
-    res.status(400).json(errors);
-  }
+      return {
+        errors,
+        isValid: isEmpty(errors)
+      };
+    });
+}
+
+router.post("/", (req, res) => {
+  validateInput(req.body, commonValidations).then(({ errors, isValid }) => {
+    if (isValid) {
+      const { username, password, email } = req.body;
+      const password_digest = bcrypt.hashSync(password, 10);
+
+      User.forge(
+        {
+          username,
+          email,
+          password_digest
+        },
+        {
+          hasTimestamps: true
+        }
+      )
+        .save()
+        .then(user => res.json({ success: true }))
+        .catch(err => res.status(500).json({ error: err }));
+    } else {
+      res.status(400).json(errors);
+    }
+  });
 });
 
 export default router;
